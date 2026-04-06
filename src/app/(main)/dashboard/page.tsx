b136e27d4ea301877dec9519c9be0c308d7f7e68
Jino <152300261+jino-code@users.noreply.github.com>
2026-04-06 22:00:21 +0900

    refactor: move header to layout (#35)

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,12 +1,5 @@
-import Header from './Header';
-
 function Dashboard() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <main>{/* コンテンツ */}</main>
-    </div>
-  );
+  return <div>dashboard</div>;
 }
 
 export default Dashboard;
---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { logout } from '../(auth)/actions';
+
+function mainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="bg-white shadow-md p-4 w-full flex justify-between items-center">
+        <Link href="/dashboard" className="text-xl font-bold hover:underline">
+          Timebox
+        </Link>
+        <form action={logout}>
+          <button className="hover:underline">ログアウト</button>
+        </form>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default mainLayout;
