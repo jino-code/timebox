f59790ab3cf23c741c6b0fc579cc1ac17857facd
Jino <152300261+jino-code@users.noreply.github.com>
2026-04-16 09:33:12 +0900

    chore: add eslint rules for no-console and import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,34 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "no-console": "warn",
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
